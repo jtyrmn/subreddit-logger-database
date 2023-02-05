0ec11761ee83b46b2e399da4f7fb3c3c2f1206dc
jtyrmn <jaketyermanbusiness@gmail.com>
2023-02-04 16:44:37 -0800

    allowed ENV file to be accessed from ENV_ATH variable

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/jtyrmn/subreddit-logger-database/database"
@@ -10,19 +11,24 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
+	envPath := ".env"
+	if e, exists := os.LookupEnv("ENV_PATH"); exists {
+		envPath = e
+	}
+
+	err := godotenv.Load(envPath)
 	if err != nil {
-		panic(err)
+		panic("cannot load env file: " + err.Error())
 	}
 
 	connection, err := database.Connect()
 	if err != nil {
-		panic(err)
+		panic("cannot connect to database: " + err.Error())
 	}
 
 	server, err := server.NewServer(connection)
 	if err != nil {
-		panic(err)
+		panic("cannot create gRPC server: " + err.Error())
 	}
 
 	log.Printf("now listening on %s\n\n", util.GetEnv("SUBREDDIT_LOGGER_DATABASE_LOCATION"))
